67979ef9f45170e074ac631dc0e6a36381c8d190
trit <trit@quantumtrip.org>
2018-06-07 15:04:27 -0400

    Draw and move @

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
 extern crate ggez;
-use ggez::*;
+extern crate nalgebra;
+
+use ggez::{
+    Context, ContextBuilder, graphics, event, GameResult, conf,
+    mouse
+};
+use ggez::event::{Keycode, Mod};
+use ggez::graphics::{Point2};
+use std::{path, env};
 
 struct State {
-    //text: graphics::Text
+    font: graphics::Font,
+    text: graphics::Text,
+    pos: Point2
+}
+
+impl State {
+    fn new(ctx: &mut Context) -> GameResult<State> {
+        let font = graphics::Font::new(ctx, "/Envy Code R.ttf", 14)?;
+        let text = graphics::Text::new(ctx, "@", &font)?;
+
+        let s = State {
+            font: font, 
+            text: text, 
+            pos: Point2::new(100.0, 100.0)
+        };
+
+        Ok(s)
+    }
+
+    fn change_pos(&mut self, d_x: f32, d_y: f32) {
+        self.pos.coords.x += d_x;
+        self.pos.coords.y += d_y;
+    }
 }
 
 impl ggez::event::EventHandler for State {
@@ -11,17 +41,30 @@ impl ggez::event::EventHandler for State {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);        
+        graphics::clear(ctx);  
+
+        graphics::draw(ctx, &self.text, *&self.pos, 0.0)?;
+
         graphics::present(ctx);
 
         Ok(())
     }
+
+    fn key_down_event(&mut self, ctx: &mut Context, keycode: Keycode, _keymod: Mod, _repeat: bool) {
+        let change_amount = 10.0;       
+
+        match keycode {
+            Keycode::Up => { self.change_pos(0.0, -change_amount); },
+            Keycode::Down => { self.change_pos(0.0, change_amount); },
+            Keycode::Left => { self.change_pos(-change_amount, 0.0); },
+            Keycode::Right => { self.change_pos(change_amount, 0.0); },
+            Keycode::Escape => { ctx.quit().expect("Should quit"); }
+            _ => { println!("{:?}", keycode); }
+        };
+    }
 }
 
-fn main() {
-    let state = &mut State {
-        
-    };
+fn main() { 
 
     let fullscreen_type = if cfg!(windows) {
         conf::FullscreenType::Off
@@ -29,15 +72,22 @@ fn main() {
         conf::FullscreenType::Desktop
     };
     
-    let ctx = &mut ContextBuilder::new("adventure", "quantumtrip")
+    let mut cb = ContextBuilder::new("adventure", "quantumtrip")
         .window_setup(conf::WindowSetup::default().title("Trit's Adventure"))
         .window_mode(conf::WindowMode::default()
-            .dimensions(1920,1080)
-            .borderless(true)
-            .fullscreen_type(fullscreen_type))
-        .build().unwrap();
+            .dimensions(1920/2,1080/2)
+            .borderless(false)
+            .fullscreen_type(fullscreen_type));
 
-    mouse::set_grabbed(ctx, true);
+    let mut resource_dir = path::PathBuf::from(env::current_dir().unwrap());
+    resource_dir.push("resources");
+    cb = cb.add_resource_path(resource_dir);
+
+
+    let ctx = &mut cb.build().unwrap();
+    mouse::set_grabbed(ctx, false);
+
+    let state = &mut State::new(ctx).unwrap();
 
     event::run(ctx, state).unwrap();
 }
