733e50f58438b9721e8c49dab5b6ecd36b6be01a
trit <trit@quantumtrip.org>
2018-06-07 08:38:55 -0400

    Full screen

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,35 @@ extern crate ggez;
 use ggez::*;
 
 struct State {
-    dt: std::time::Duration
+    //text: graphics::Text
 }
 
 impl ggez::event::EventHandler for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.dt = timer::get_delta(ctx);
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {        
         Ok(())
     }
 
-    fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);        
+        graphics::present(ctx);
+
         Ok(())
     }
 }
 
 fn main() {
     let state = &mut State {
-        dt: std::time::Duration::new(0, 0)
+        
     };
-    let conf = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("adventure", "trit", conf).unwrap();
+    
+    let ctx = &mut ContextBuilder::new("adventure", "quantumtrip")
+        .window_setup(conf::WindowSetup::default().title("Trit's Adventure"))
+        .window_mode(conf::WindowMode::default()
+            .dimensions(1920,1080)
+            .borderless(true))
+        .build().unwrap();
+
+    mouse::set_grabbed(ctx, true);
 
     event::run(ctx, state).unwrap();
 }
