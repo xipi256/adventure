6cd06a26115f9caf81607f4e21b555b7d6d9c190
trit <trit@quantumtrip.org>
2018-06-20 13:16:02 -0400

    Move the hero

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate ggez;
 
 use ggez::{conf, Context, ContextBuilder, event, GameResult, graphics};
 use ggez::event::{KeyCode, KeyMods};
-
+use ggez::nalgebra as na;
 
 struct MainState {
     hero: Hero
@@ -29,6 +29,10 @@ impl Hero {
     fn draw(&self, ctx: &mut Context) -> GameResult {
         graphics::rectangle(ctx, [1.0, 0.55, 0.0, 1.0].into(), graphics::DrawMode::Fill, *&self.rect)
     }
+
+    fn change_pos(&mut self, d_x: f32, d_y: f32) {
+        self.rect.translate(na::base::Vector2::new(d_x, d_y))
+    }
 }
 
 impl ggez::event::EventHandler for MainState {
@@ -38,7 +42,7 @@ impl ggez::event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.0, 0.0, 0.0, 0.0].into());
-        &self.hero.draw(ctx)?;
+        self.hero.draw(ctx)?;
         graphics::present(ctx)
     }
 
@@ -49,7 +53,32 @@ impl ggez::event::EventHandler for MainState {
         _keymods: KeyMods,
         _repeat: bool,
     ) {
+        let change_amount = 10.0;
         match keycode {
+            KeyCode::K | KeyCode::Up | KeyCode::Numpad8 => { 
+                self.hero.change_pos(0.0, -change_amount)
+            }
+            KeyCode::J | KeyCode::Down | KeyCode::Numpad2 => { 
+                self.hero.change_pos(0.0, change_amount)
+            }
+            KeyCode::H | KeyCode::Left | KeyCode::Numpad4 => { 
+                self.hero.change_pos(-change_amount, 0.0)
+            }
+            KeyCode::L | KeyCode::Right | KeyCode::Numpad6 => { 
+                self.hero.change_pos(change_amount, 0.0)
+            }
+            KeyCode::Y | KeyCode::Numpad7 => {
+                self.hero.change_pos(-change_amount, -change_amount)
+            }
+            KeyCode::U | KeyCode::Numpad9 => {
+                self.hero.change_pos(change_amount, -change_amount)
+            }
+            KeyCode::B | KeyCode::Numpad1 => {
+                self.hero.change_pos(-change_amount, change_amount)
+            }
+            KeyCode::N | KeyCode::Numpad3 => {
+                self.hero.change_pos(change_amount, change_amount)
+            }
             KeyCode::Escape => ctx.quit(),
             _ => println!("{:?}", keycode)
         }
